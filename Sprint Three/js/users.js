748a8435a27be970b3ade335327c4c7302717ce8
Benjamin Stafford <30082213@tafe.wa.edu.au>
2024-05-10 19:25:20 +0800

    Update users.js

--- Sprint Three/js/users.js
+++ Sprint Three/js/users.js
@@ -3,64 +3,53 @@
 	Date: 09/05/2024*/
 
 $(document).ready(function () {
-    $(document).on("submit", "#form-newsletter", function(event) {
+    $("#subscribe-button").click(function(event) {
         event.preventDefault();
-
-        var buttonId = $(document.activeElement).attr('id');
-
-        // if subscribe button was pressed
-        if (buttonId === "subscribe-button") {
-            $.ajax({
-                url: "request/user_obj.php",
-                type: "POST",
-                dataType: "json",
-                data: new FormData(this),
-                processData: false,
-                contentType: false,
-                
-                success: function (response) {
-                    console.log(response);
-                    if (response) {
-                        alert("Subscription successful");
-                        window.location.href = `index.php`;
-                    }
-                },
-                error: function(xhr, ajaxOptions, thrownError) {
-                    console.log(xhr.status);
-                    console.log(thrownError);
+        $.ajax({
+            url: "request/user_obj.php",
+            type: "POST",
+            dataType: "json",
+            data: new FormData($("#form-newsletter")[0]),
+            processData: false,
+            contentType: false,
+            success: function (response) {
+                console.log(response);
+                if (response) {
+                    alert("Subscription successful");
+                    window.location.href = "index.php";
                 }
-            });
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+                console.log(xhr.status);
+                console.log(thrownError);
+            }
+        });
+    });
 
-            //if remove button was pressed
-        } else if (buttonId === "remove-button") {
-
-            var email = $("#email").val();
-
-            $.ajax({
-                url: "request/user_obj.php",
-                type: "POST",
-                dataType: "json",
-                data: {email: email, action: "remove" },
-                processData: false,
-                contentType: false,
-                
-                success: function (response) {
-                    console.log(response);
-                    if (response) {
-                        alert("Account removed successfully");
-                        window.location.href = `index.php`;
-                    }
-                    else
-                    {
-                        alert(response);
-                    }
-                },
-                error: function(xhr, ajaxOptions, thrownError) {
-                    console.log(xhr.status);
-                    console.log(thrownError);
+    $("#remove-button").click(function(event) {
+        event.preventDefault();
+        var email = $("#input-email").val();
+        $.ajax({
+            url: "request/user_obj.php",
+            type: "POST",
+            dataType: "json",
+            data: { email: email, action: "remove" },
+            success: function (response) {
+                console.log(response);
+                if (response) {
+                    alert("Account removed successfully");
+                    window.location.href = "index.php";
+                } else {
+                    alert("Error removing account");
                 }
-            });
-        }
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+                console.log(xhr.status);
+                console.log(thrownError);
+            }
+        });
     });
 });
+    
+    
     
